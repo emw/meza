c64d7073472ce693a4597c55ba05bfa69e33c36b
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-10-13 12:50:52 -0500

    Made initial script actually work (min viable)

--- scripts/uniteTheWikis.php
+++ scripts/uniteTheWikis.php
@@ -65,6 +65,7 @@ class UniteTheWikis extends Maintenance {
 	protected $fileMove;
 	protected $pages;
 	protected $db;
+	protected $maintDir = '/opt/meza/htdocs/mediawiki/maintenance/';
 
 	public function __construct () {
 
@@ -75,6 +76,7 @@ class UniteTheWikis extends Maintenance {
 		$this->fileDisambig = "$DIR/disambig.mediawiki";
 		$this->fileXml = "$DIR/mwTransfer.xml";
 		$this->fileMove = "$DIR/mwMovePage.txt";
+		$this->fileDumpList = "$DIR/mwDumpList.txt";
 
 
 		// addOption ($name, $description, $required=false, $withArg=false, $shortName=false)
@@ -95,16 +97,28 @@ class UniteTheWikis extends Maintenance {
 		$this->mergedwiki = $this->getOption( 'mergedwiki' );
 		$this->sourcewikis = explode( ',', $this->getOption( 'sourcewikis' ) );
 
-		$this->getPages()
-		// $this->doImport();
+		$this->getPages();
 
 	}
 
-	public function importUniquePage ($pagename, $wiki) {
+	public function dumpPageXML ($pages, $wiki) {
+		unlink( $this->fileDumpList );
+
+		if ( is_array($pages) ) {
+			$pagelist = implode( "\n", $pages );
+		}
+		else {
+			$pagelist = $pages;
+		}
+		file_put_contents( $this->fileDumpList, $pagelist );
+		shell_exec( "WIKI=$wiki php {$this->maintDir}dumpBackup.php --full --pagelist={$this->fileDumpList} > {$this->fileXml}" );
+	}
+
+	public function importUniquePage ($pages, $wiki) {
 
 		unlink( $this->fileXml );
-		shell_exec( "WIKI=$wiki php dumpBackup.php \"$pagename\" > {$this->$fileXml}" );
-		shell_exec( "WIKI={$this->mergedwiki} php importDump.php < {$this->$fileXml}" );
+		$this->dumpPageXML( $pages, $wiki );
+		shell_exec( "WIKI={$this->mergedwiki} php {$this->maintDir}importDump.php < {$this->fileXml}" );
 
 		return;
 
@@ -144,15 +158,15 @@ class UniteTheWikis extends Maintenance {
 			unlink( $fileMove );
 			$wikiForTitle = strtoupper( $wiki );
 			file_put_contents( $fileMove, "$pagename|$pagename ($wikiForTitle)" );
-			shell_exec( "WIKI=$wiki php dumpBackup.php \"$pagename\" > $fileXml" );
-			shell_exec( "WIKI=$mergedwiki php importDump.php < $fileXml" );
-			shell_exec( "WIKI=$mergedwiki php moveBatch.php --noredirects -r \"$deconflictMsg\" $fileMove" );
+			$this->dumpPageXML( $pagename, $wiki );
+			shell_exec( "WIKI=$mergedwiki php {$this->maintDir}importDump.php < $fileXml" );
+			shell_exec( "WIKI=$mergedwiki php {$this->maintDir}moveBatch.php --noredirects -r \"$deconflictMsg\" $fileMove" );
 			$disambigForTemplate .= "$pagename ($wikiForTitle)\n";
 		}
 
 		$disambigForTemplate .= "}}";
 		file_put_contents( "$fileDisambig", $disambigForTemplate );
-		shell_exec( "WIKI=$mergedwiki php edit.php -s \"$disambigMsg\" \"$pagename\" < $fileDisambig" );
+		shell_exec( "WIKI=$mergedwiki php {$this->maintDir}edit.php -s \"$disambigMsg\" \"$pagename\" < $fileDisambig" );
 
 		return;
 
@@ -178,39 +192,100 @@ class UniteTheWikis extends Maintenance {
 			SELECT
 				page_namespace,
 				page_title,
+				COUNT( * ) AS num_wikis,
 				COUNT( distinct texthash ) AS uniques,
-				GROUP_CONCAT(wiki) AS wikis,
+				GROUP_CONCAT(wiki) AS wikis
 			FROM (
 				$union
 			) AS tmp
-			GROUP BY page_namespace, page_title";
+			GROUP BY page_namespace, page_title
+			ORDER BY page_namespace DESC, wiki";
 
-		echo $query;
+		// echo $query;
 
 		$dbr = wfGetDB( DB_SLAVE );
 
+		$this->output( "\nStarting import\n===============\n" );
+
 		$result = $dbr->query( $query );
+		$importQueue = array();
+
+
 		while( $page = $result->fetchObject() ) {
-			$this->handleImport( $page );
+
+			$this->output( "\nNext DB row. Wikis=" . $page->wikis
+				. "; NS=" . $page->page_namespace
+				. "; title=" . $page->page_title );
+
+			// get the current wiki being put into the queue. If the queue has
+			// pages in it, grab the `wikis` property of any page (they're all
+			// the same). If the queue is empty, set the queue-wiki to the wiki
+			// of the current loop
+			$importQueueWiki = count($importQueue) > 0 ? $importQueue[0]->wikis : $page->wikis;
+
+			if ( intval($page->num_wikis) ===  1 && $importQueueWiki === $page->wikis ) {
+				$this->output( "\n  --> Queue" );
+				$importQueue[] = $page;
+			}
+			else {
+				// Import the pages in the queue and clear it
+				if ( count( $importQueue ) > 0 ) {
+					$this->output( "\n\nProcess queue..." );
+					$this->handleImport( $importQueue );
+					$importQueue = array();
+				}
+
+				// Do this import
+				$this->output( "\nHandling import for page " . $page->page_title );
+				$this->handleImport( $page );
+			}
+		}
+
+		// if the while-loop ended with some in the queue, import them
+		if ( $importQueue !== null ) {
+			$this->handleImport( $importQueue );
 		}
 
 		return;
 
 	}
 
-	public function handleImport ( $page ) {
+	public function handleImport ( $pages ) {
 
-		$wikis = explode( ",", $page->wikis );
-		$pageTitleObj = Title::makeTitle( $page->page_namespace, $page->page_title );
-		$pageTitleText = $pageTitleObj->getFullText();
+		// Determine if this is multiple pages or just one
+		if ( is_array($pages) && count($pages) > 1 ) {
+			// if multiple pages, they'll all be from the same wiki
+			$wikis = array( $pages[0]->wikis );
+			$pageTitleText = array();
+			$this->output( "\nImporting multiple pages from " . $pages[0]->wikis . ": ");
+			foreach( $pages as $page ) {
+				$pageTitleObj = Title::makeTitle( $page->page_namespace, $page->page_title );
+				$text = $pageTitleObj->getFullText();
+				$pageTitleText[] = $text;
+				$this->output( "\n  * $text" );
+			}
+		}
+		else {
+			if ( is_array($pages) && count($pages) === 1 ) {
+				$pages = $pages[0];
+			}
+			$wikis = explode( ",", $pages->wikis );
+			$pageTitleObj = Title::makeTitle( $pages->page_namespace, $pages->page_title );
+			$pageTitleText = $pageTitleObj->getFullText();
+			$this->output( "\nImporting page $pageTitleText from " . $pages->wikis );
+		}
+
 
 		if ( count( $wikis ) === 1 ) {
+			$this->output( "\n\nImport unique page(s)\n" );
 			$this->importUniquePage( $pageTitleText, $wikis[0] );
 		}
-		else if ( $page->uniques === 1 ) {
+		else if ( $pages->uniques === 1 ) {
+			$this->output( "\n\nImport identical pages\n" );
 			$this->importIdenticalPages( $pageTitleText, $wikis );
 		}
 		else {
+			$this->output( "\n\nImport conflicted pages\n" );
 			$this->importConflictedPages( $pageTitleText, $wikis );
 		}
 
